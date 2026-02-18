1edc6ed8e450ea6550b4c26394649d4c76e3cd3f
Teïlo Millet <teilomillet@gmail.com>
2026-02-18 21:46:03 +0100

    Initial cask formula and auto-update workflow

---
+++ Casks/mime.rb
@@ -0,0 +1,11 @@
+cask "mime" do
+  version "0.1.0"
+  sha256 :no_check
+
+  url "https://github.com/teilomillet/mime/releases/download/v#{version}/Mime_#{version}_aarch64.dmg"
+  name "Mime"
+  desc "A distraction-free note-taking app"
+  homepage "https://github.com/teilomillet/mime"
+
+  app "Mime.app"
+end
