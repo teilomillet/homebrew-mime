bd9a220f362d254dc4a8afd50f128a529cb261fc
Teïlo Millet <teilomillet@gmail.com>
2026-02-18 23:13:35 +0100

    Update mime to 0.1.2

--- Casks/mime.rb
+++ Casks/mime.rb
@@ -1,8 +1,8 @@
 cask "mime" do
-  version "0.1.1"
+  version "0.1.2"
   sha256 :no_check
 
-  url "https://github.com/teilomillet/mime/releases/download/v#{version}/Mime_0.1.0_aarch64.dmg"
+  url "https://github.com/teilomillet/mime/releases/download/v#{version}/Mime_#{version}_aarch64.dmg"
   name "Mime"
   desc "A distraction-free note-taking app"
   homepage "https://github.com/teilomillet/mime"
